99cd5369289348912b19156d25ecbb33f67d28fe
David Higgins <david@gryphonandrook.com>
2016-06-13 11:17:28 -0600

    Setup & Start ElasticSearch

--- recipes/install.rb
+++ recipes/install.rb
@@ -15,3 +15,16 @@ end
 dpkg_package "/tmp/elasticsearch-#{node[:elasticsearch][:version]}.deb" do
   action :install
 end
+
+execute 'start_elasticsearch' do
+  command '/etc/init.d/elasticsearch start'
+  user 'root'
+  action :nothing
+end
+
+execute 'setup service' do
+  command 'update-rc.d elasticsearch defaults 95 10'
+  user 'root'
+  action :run
+  notifies :run, 'execute[start_elasticsearch]'
+end
